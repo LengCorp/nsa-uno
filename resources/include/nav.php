cab7cd609b102aec05c8aa3aacd76c7bf7358bf7
Emil Lengman <emillen@kth.se>
2016-04-21 15:57:01 +0200

    Basic header, footer, and navbar added

---
+++ resources/include/nav.php
@@ -0,0 +1,29 @@
+
+<nav class="navbar navbar-default">
+	<div class="container thinner">
+		<div class="navbar-header">
+			
+		  	<button type="button" class="navbar-toggle collapsed" 
+		  		data-toggle="collapse" data-target="#navbar" 
+		  		aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				
+		  	</button>
+			
+	  	</div>
+		<div id="navbar" class="collapse navbar-collapse container">
+			<ul class="nav navbar-nav ">
+				<li <?php if($page == 'hem') echo 'class="active"' ?>>
+					<a href=".">Hem</a>
+				</li>
+			    <li>
+            <a href="historik.php">Historik</a>
+          </li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
