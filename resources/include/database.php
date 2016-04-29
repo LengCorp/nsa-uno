3840d468a4a54b23314d70f22e5f0197e31207e1
Simon <simon@temtron.se>
2016-04-29 14:31:19 +0200

    created extension for history to call DatabaseSelect

--- resources/include/database.php
+++ resources/include/database.php
@@ -19,28 +19,71 @@ function DatabaseConnect(){
     return $conn;
 }
 
-function DatabaseSelect(){
+function DatabaseSelect($DBpage){
 
     $conn = DatabaseConnect();
 
-    $sql = "SELECT event.id, time, eventtype.type FROM event JOIN eventtype on event.type = eventtype.id ORDER BY event.id DESC";
-    $result = $conn->query($sql);
+    if ($DBpage == "index") {
+        $sql = "SELECT time, eventtype.type FROM event JOIN eventtype on event.type = eventtype.id WHERE eventtype.type = 'ON' OR eventtype.type = 'OFF' ORDER BY event.id DESC";
+        $result = $conn->query($sql);
 
-    if ($result) {
-        // output data of each row
-        echo "<div class='container'>";
-        echo "<table class='table table-striped'>";
-        echo "<thead>";
-        echo "<tr><th>id</th><th>Time</th><th>Type</th></tr>";
-        echo "<tbody>";
-        while($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>" . $row["id"]. "</td><td>" . $row["time"]. "</td><td>" . $row["type"]. "</td>";
-            echo "</tr>";
+        if ($result) {
+            echo "<div class='container'>";
+            echo "<table class='table table-striped'>";
+            echo "<thead>";
+            echo "<tr><th>Time</th><th>Status</th></tr>";
+            echo "<tbody>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row["time"] . "</td><td>" . $row["type"] . "</td>";
+                echo "</tr>";
+                break;
+            }
+            echo "</div>";
+        } else {
+            echo "0 results";
         }
-        echo "</div>";
-    } else {
-        echo "0 results";
+    }
+
+    else if($DBpage == "history"){
+        $sql = "SELECT event.id, time, eventtype.type FROM event JOIN eventtype on event.type = eventtype.id ORDER BY event.id ASC";
+        $result = $conn->query($sql);
+
+        if ($result) {
+
+            echo "<div class='container'>";
+            echo "<table class='table table-striped'>";
+            echo "<thead>";
+            echo "<tr><th>id</th><th>Time</th><th>Status</th></tr>";
+            echo "<tbody>";
+
+            $event = [];
+            $i = 0;
+
+            while ($row = $result->fetch_assoc()) {
+                $event[$i]["id"] = $row["id"];
+                $event[$i]["time"] = $row["time"];
+                $event[$i]["type"] = $row["type"];
+                $i++;
+            }
+
+            //decide color for the output
+            $color = "black";
+            for($i = 0; $i < sizeof($event); $i++){
+                if($event[$i]["type"] == "ON")
+                    $color = "red";
+                else if($event[$i]["type"] == "OFF")
+                    $color = "green";
+                echo "<tr style='color: $color; font-weight: bold;'>";
+                echo "<td>" . $event[$i]["id"] . "</td><td>" . $event[$i]["time"] . "</td><td>" . $event[$i]["type"] . "</td>";
+                echo "</tr>";
+            }
+
+
+            echo "</div>";
+        } else {
+            echo "0 results";
+        }
     }
 
     $conn->close();
