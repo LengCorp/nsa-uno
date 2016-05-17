24b0dd053287caa701324843699b1006b92acdb1
Simon <simon@temtron.se>
2016-05-17 12:13:46 +0200

    code to check if there is a new entry in the DB

--- resources/include/checkdata.php
+++ resources/include/checkdata.php
@@ -1,1 +1,15 @@
 <?php
+include('database.php');
+session_start();
+
+$conn = databaseConnect();
+$sql = "SELECT time, eventtype.type, eventtype.id FROM event JOIN eventtype ON event.type = eventtype.id ORDER BY event.id DESC";
+$result = $conn->query($sql);
+
+if ($result) {
+    $row = $result->fetch_assoc();
+    $time = $row["time"];
+    if ($time > $_SESSION["timeOfStatus"]) {
+        echo "true";
+    }
+}
