b2ad7ffd1c868eac44d8689d6c2e3ad0a327a55b
Simon <simon@temtron.se>
2016-05-17 12:14:56 +0200

    ajax to check if there is a new entry

--- resources/js/nsa-uno.js
+++ resources/js/nsa-uno.js
@@ -1,6 +1,6 @@
 function DatabaseInsert(type) {
     $.ajax({
-        url: "resources/include/database.php?insert=" + type,
+        url: "resources/include/databaseFunction.php?insert=" + type,
         context: document.body
     }).done(function (result) {
         $(".index_time").append("<div class='temp'>" + result + "</div>").html($(".index_time_source").html());
@@ -20,6 +20,12 @@ function DatabaseInsert(type) {
 
 setInterval(function(){
     $.ajax({
-        url: "resources/include/checkdata.php"
+        url: "resources/include/checkdata.php",
+        context: document.body,
+        success: function(data){
+            if (data == "true"){
+                location.reload();
+            }
+        }
     });
-}, 5000);
+}, 4000);
